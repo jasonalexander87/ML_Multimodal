e7e69b0cac2c3d805e598b4ccf877307ba063113
jasonalexander87 <65502810+jasonalexander87@users.noreply.github.com>
2022-06-24 02:06:41 +0300

    Add files via upload

---
+++ Demos/js/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.tflite_js;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.webkit.WebViewAssetLoader;
+import androidx.webkit.WebViewClientCompat;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Configure WebView
+        final WebViewAssetLoader assetLoader = new WebViewAssetLoader.Builder()
+                .addPathHandler("/assets/", new WebViewAssetLoader.AssetsPathHandler(this))
+                .addPathHandler("/res/", new WebViewAssetLoader.ResourcesPathHandler(this))
+                .build();
+
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.getAllowFileAccess();
+        webSettings.setAllowFileAccessFromFileURLs(true);
+        webSettings.setAllowUniversalAccessFromFileURLs(true);
+
+
+        myWebView.setWebViewClient(new LocalContentWebViewClient(assetLoader));
+
+        // Load html
+        myWebView.loadUrl("https://appassets.androidplatform.net/assets/index.html");
+
+    }
+}
+
